11532cbaf05b3d97d8caf1207f8d80890682d93d
JoaoBarros93 <ec11242@fe.up.pt>
2017-10-30 23:20:25 +0000

    Add files via upload

--- src/APIC.cpp
+++ src/APIC.cpp
@@ -83,11 +83,14 @@ bool APIC::login(int n, APIC apic) {
 bool APIC::regist(APIC apic) {
 	string username, password, institution, areaCompleteString;
 	vector<string> areaVector;
-	int pick;
+	int pick, pick2;
+	string area, subarea;
+	bool exit=false,exit2=false;
 	stringstream ss;
 	stringstream areaComplete;
 
-	cout << "Select your username (semicolon or comma will exit): " << endl;
+	cout << endl << "Select your username (semicolon or comma will exit): " << endl;
+	cout << "Username: ";
 	cin >> username;
 	cin.clear();
 	cin.ignore(10000, '\n');
@@ -104,7 +107,8 @@ bool APIC::regist(APIC apic) {
 		}
 	}
 
-	cout << "Select your password (semicolon or comma will exit): " << endl;
+	cout << endl <<"Select your password (semicolon or comma will exit): " << endl;
+	cout << "Password: ";
 	cin >> password;
 	cin.clear();
 	cin.ignore(10000, '\n');
@@ -112,7 +116,8 @@ bool APIC::regist(APIC apic) {
 	if (findSemicolon(password))
 		menu1(apic);
 
-	cout << "Select your institution (semicolon or comma will exit): " << endl;
+	cout << endl << "Select your institution (semicolon or comma will exit): " << endl;
+	cout << "Institution: ";
 	cin >> institution;
 	cin.clear();
 	cin.ignore(10000, '\n');
@@ -121,66 +126,80 @@ bool APIC::regist(APIC apic) {
 		menu1(apic);
 
 	do {
-		string area, subarea;
-		int pick2;
-		cout << "How do you want to visualize the areas of interest?" << endl;
+		do{
+
+		cout << endl << "How do you want to visualize the areas of interest?" << endl;
 		cout << "1: The full name" << endl;
 		cout << "2: Just siglas" << endl;
 		cout << "3: I dont want to visualize it" << endl;
+		cout << "Your pick: ";
 		cin >> pick2;
 		cin.clear();
 		cin.ignore(10000, '\n');
+		cout << endl;
 
 		switch (pick2) {
 		case 1:
 			printAreasFull();
+			exit=true;
 			break;
 		case 2:
 			printAreasSiglas();
+			exit=true;
 			break;
 		case 3:
+			exit=true;
 			break;
 		default:
-			cout << "Wrong input!";
+			cout << "Wrong input!" << endl << endl;
 			break;
 		}
+		}while(exit==false);
 
-		cout << "Please choose the area you want to add: " << endl;
+		do{
+		cout << endl << "Please choose the area you want to add: " << endl;
 		cout << "Your area: ";
 		cin >> area;
 		cin.clear();
 		cin.ignore(10000, '\n');
+		if (findSemicolon(area))
+				menu1(apic);
+
+
 
 		if (checkArea(area)) {
+			cout << endl << "Now please pick a sub area: " << endl;
 			printSubArea(area);
-			cout << "Now please pick a sub area: " << endl;
+			cout << endl;
 			cout << "Your sub area: ";
 			cin >> subarea;
 			cin.clear();
 			cin.ignore(10000, '\n');
+			if (findSemicolon(subarea))
+					menu1(apic);
 			if (checkSubArea(subarea)) {
 				cout << "Your area was added with sucess! " << endl;
+				exit=true;
 				areaComplete.clear();
 				areaComplete << area << "-" << subarea;
 				areaComplete >> areaCompleteString;
 				areaVector.push_back(areaCompleteString);
-				cout
-						<< "Do you want to add another area (1 for yes, anything else for no) ?"
-						<< endl;
+				cout << "Do you want to add another area (1 for yes, anything else for no) ?" << endl;
 				cout << "Your pick: ";
 				cin >> pick;
 				cin.clear();
 				cin.ignore(10000, '\n');
 			} else {
 				cout << "Could not find your sub area ! " << endl;
-				return false;
 			}
 		} else {
 			cout << "Could not find your area ! " << endl;
-			return false;
 		}
 
+
 	} while (pick == 1);
+		if(pick!=1)exit2=true;
+	}while(exit2==false);
 
 	ss << username << ";";
 	ss << password << ";";
@@ -255,6 +274,7 @@ void APIC::searchUser() {
 	string user;
 	cout << "Please, insert the login name of the user you are trying to find: "
 			<< endl;
+	cout << "Your pick: ";
 	cin >> user;
 	cin.clear();
 	cin.ignore(10000, '\n');
@@ -275,6 +295,7 @@ void APIC::searchUser() {
 }
 
 void APIC::printAreasFull() {
+	cout << "----AREAS----" << endl;
 	for (unsigned int i = 0; i < areas.size(); i++) {
 		if (i != 0) {
 			if (areas[i].getAreaName() != areas[i - 1].getAreaName())
@@ -285,6 +306,7 @@ void APIC::printAreasFull() {
 }
 
 void APIC::printAreasSiglas() {
+	cout << "----AREAS----" << endl;
 	for (unsigned int i = 0; i < areas.size(); i++) {
 		if (i != 0) {
 			if (areas[i].getAreaNameSigla() != areas[i - 1].getAreaNameSigla())
@@ -296,7 +318,7 @@ void APIC::printAreasSiglas() {
 void APIC::printSubArea(string area) {
 	for (unsigned int i = 0; i < areas.size(); i++) {
 		if (areas[i].getAreaNameSigla() == area) {
-			cout << areas[i].getSubAreaName() << "-"
+			cout << areas[i].getSubAreaName() << " - "
 					<< areas[i].getSubAreaNameSigla() << endl;
 		}
 	}
@@ -322,6 +344,7 @@ void APIC::searchUserByArea() {
 	string area;
 	vector<string> usersVector;
 	cout << "Please insert the sigla of the area you are looking for: " << endl;
+	cout << "Your pick: ";
 	cin >> area;
 	cin.clear();
 	cin.ignore(10000, '\n');
@@ -390,7 +413,7 @@ bool APIC::findSemicolon(string stringToSearch) {
 	index3 = stringToSearch.find(' ');
 
 	if (index1 != npos || index2 != npos || index3 != npos) {
-		cout << "ERROR! Wrong input! " << endl;
+		cout << "You returned back! " << endl <<endl;
 		return true;
 	} else
 		return false;
@@ -398,7 +421,9 @@ bool APIC::findSemicolon(string stringToSearch) {
 
 void APIC::printSubAreasUser() {
 	string area;
-	cout << "What is the area you want to look for sub areas (type ; for return)?";
+	cout
+			<< "What is the area you want to look for sub areas (type ; for return)?";
+	cout << "Your pick: ";
 	cin >> area;
 	cin.clear();
 	cin.ignore(10000, '\n');
@@ -457,7 +482,8 @@ void APIC::menu2(APIC apic) {
 		cout << "3: Search for an user by its name" << endl;
 		cout << "4: Search for all users in an area" << endl;
 		cout << "5: Search for all users in an subarea" << endl;
-		cout << "6: Print all areas of interest with complete information " << endl;
+		cout << "6: Print all areas of interest with complete information "
+				<< endl;
 		cout << "7: Print all areas of interest, but just siglas " << endl;
 		cout << "8: Look for the subareas of an area" << endl;
 		cout << "9: Search for an event " << endl;
